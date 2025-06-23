777a99739b92c844b2198cbef8357e27edae5986
wivwiv <bigpar@qq.com>
2025-06-23 17:59:00 +0800

    适配云端

--- api/config.js
+++ api/config.js
@@ -3,7 +3,8 @@ module.exports = {
     endpoint: 'http://127.0.0.1:8081/handleEncryption'
   },
   mysql: {
-    host: process.env.MYSQL_HOST || 'mysql-idea'
+    host: process.env.MYSQL_HOST || 'mysql-idea',
+    port: 3306
   },
   proxyHost:  process.env.PROXY_HOST || 'http://qos-msg-idea:18083',
   jwtSecret: "emqxsecret"
